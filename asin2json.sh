2bc56db449df0550f98ec6066178f0e35a75cfe5
Sridhar Sarnobat <ss401533@gmail.com>
2018-06-27 21:43:04 -0700

    Minor changes (untested)

--- asin2json.sh
+++ asin2json.sh
@@ -1,2 +1,6 @@
+: ${AWS_ACCESS_KEY_ID?"AWS_ACCESS_KEY_ID not set"}
+: ${AWS_SECRET_ACCESS_KEY?"AWS_SECRET_ACCESS_KEY not set"}
+: ${1?"no ASIN passed"}
+ASIN=$1
 which xml2json >/dev/null || pip install https://github.com/hay/xml2json/zipball/master
-echo "${1:-B01N1X82AU}" | xargs groovy product2url.groovy | xargs curl --silent | xml2json | perl -pe 's/\{http.*?2011-08-01\}//g' | jq '.' 
+echo $ASIN | xargs groovy product2url.groovy | xargs curl --silent | xml2json | perl -pe 's/\{http.*?2011-08-01\}//g' | jq '.' 
